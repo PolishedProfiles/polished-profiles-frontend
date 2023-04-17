3d110baf46d6d86c503fd37748838b9b35daf0b1
John Chavez <Jrc2855@gmail.com>
2023-04-17 19:01:14 +0000

    Getting unstyled data in App navbar link.

--- src/Components/ResumeEntry/index.jsx
+++ src/Components/ResumeEntry/index.jsx
@@ -9,17 +9,26 @@ function DataEntry() {
   async function handleSubmit(event) {
     event.preventDefault();
     // console.log(userResume);
-    try{
-      let response = await axios.post('http://localhost:3001/api/resume', userResume);
+    try {
+      let response = await axios.post('http://localhost:3001/api/resume', userResume, {
+        headers: {
+          'Content-Type': 'text/plain'
+        }
+      });
+      // axios.post('http://localhost:3001/api/resume', userResume);
       console.log(response.data);
       setGeneratedResume(response.data);
-    }catch(error){
+    } catch (error) {
       console.log(error.message);
     }
   }
 
   function handleChange(event) {
     setUserResume(event.target.value)
+    setTimeout(() => {
+      console.log(userResume);
+
+    }, 500);
 
   }
   return (
