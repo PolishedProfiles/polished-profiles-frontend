9e72fbd629bc907a833e04db2cfca342f0ea8beb
John Chavez <Jrc2855@gmail.com>
2023-04-17 18:33:30 +0000

    Do not merge! Work in progress!

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LandingPage from './Components/LandingPage';
 import Navbar from './Components/Navbar';
+import DataEntry from './Components/ResumeEntry';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
           <Route path='/history' element={<h1> History </h1>} />
           <Route path='/about' element={<h1> About </h1>} />
           <Route path='/login' element={<h1> Login </h1>} />
-          <Route path='/App' element={<h1> App </h1>} />
+          <Route path='/App' element={<DataEntry />} />
         </Routes>
       </div>
     </Router>
---
+++ src/Components/ResumeEntry/index.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import axios from "axios";
+
+
+function DataEntry() {
+  const [userResume, setUserResume] = useState();
+  const [generatedResume, setGeneratedResume] = useState();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    // console.log(userResume);
+    try{
+      let response = await axios.post('http://localhost:3001/api/resume', userResume);
+      console.log(response.data);
+      setGeneratedResume(response.data);
+    }catch(error){
+      console.log(error.message);
+    }
+  }
+
+  function handleChange(event) {
+    setUserResume(event.target.value)
+
+  }
+  return (
+    <div className='ResumeEntry' style={{ display: "flex" }}>
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <label>Input Info
+          <textarea style={{ width: '500px', height: '500px' }}
+            onChange={(event) => handleChange(event)}>
+          </textarea>
+        </label>
+        <button>
+          Generate
+        </button>
+      </form>
+      <p>
+        {generatedResume}
+      </p>
+    </div>
+  )
+}
+
+export default DataEntry;
