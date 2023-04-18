30ff88d4053c78eb02b3021be8ae6eca6747d105
John Chavez <Jrc2855@gmail.com>
2023-04-18 18:11:58 +0000

    History page scaffolding

--- src/Components/History/index.jsx
+++ src/Components/History/index.jsx
@@ -1,8 +1,21 @@
+import { Grid } from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
 import Typography from '@mui/material/Typography';
 import React from 'react';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 function History() {
   const [open, setOpen] = React.useState(false);
@@ -10,23 +23,29 @@ function History() {
   const handleClose = () => setOpen(false);
 
   return (
-    <div>
-      <Button onClick={handleOpen}>Open Modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
+      <Grid
+        container
+        direction="column"
       >
-      <Box>
-        <Typography>
-          Header Text in a Modal
-        </Typography>
-        <Typography>
-          Body Text in a Modal
-        </Typography>
-      </Box>
-
-      </Modal>
-    </div>
+        <Button onClick={handleOpen}>
+          Job Application no.1
+        </Button>
+        <Button>Job Application no.2</Button>
+        <Button>Job Application no.3</Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+        >
+          <Box sx={style}>
+            <Typography>
+              Header Text in a Modal
+            </Typography>
+            <Typography>
+              Body Text in a Modal
+            </Typography>
+          </Box>
+        </Modal>
+      </Grid>
   )
 };
 
