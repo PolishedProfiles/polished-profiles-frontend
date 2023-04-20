621898e128edf2a36e4a547eadd69f303171ad84
John Chavez <Jrc2855@gmail.com>
2023-04-20 18:19:05 +0000

    Removed unused assets from history import

--- src/Components/History/index.jsx
+++ src/Components/History/index.jsx
@@ -1,4 +1,4 @@
-import { Backdrop, Card, CardMedia, Fade, Grid } from '@mui/material';
+import { Backdrop, Card, Fade, Grid } from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
